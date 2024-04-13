016a661bf6950d32d5eaf32ae0f3b1e6057f89af
Thayer Anderson <tanderson11@gmail.com>
2024-04-12 21:26:38 -0400

    Better fix.

--- tag.php
+++ tag.php
@@ -12,8 +12,7 @@
 get_header(); ?>
 
 <?php
-//$tag_title = single_tag_title();
-$tag_title = 'On Gaza';
+$tag_title = single_tag_title($display=false);
 $tag_posts = new WP_Query(array('tag' => $tag_title, 'posts_per_page' => 10));
 ?>
 
